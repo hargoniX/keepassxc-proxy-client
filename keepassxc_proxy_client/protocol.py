40a0ceac9baca4c44b7a2f13f04e524e67b1e67c
Marcel Lauhoff <ml@irq0.org>
2022-01-16 16:24:53 +0100

    Support > 4096 responses

--- keepassxc_proxy_client/protocol.py
+++ keepassxc_proxy_client/protocol.py
@@ -161,7 +161,19 @@ class Connection:
             return response["entries"]
 
     def get_unencrypted_response(self):
-        return json.loads(self.socket.recv(4096).decode("utf-8"))
+        data = []
+        while True:
+            try:
+                new_data = self.socket.recv(4096)
+                if new_data:
+                    data.append(new_data.decode('utf-8'))
+                else:
+                    break
+                if len(new_data) < 4096:
+                    break
+            except:
+                pass
+        return json.loads(''.join(data))
 
     def get_encrypted_response(self):
         raw_response = self.get_unencrypted_response()
